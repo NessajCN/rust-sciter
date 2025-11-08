5ad561728d01916098e55c56f404ed6181d40c47
Nessaj <ksnessaj@hotmail.com>
2025-11-08 18:51:41 +0800

    fix

--- src/eventhandler.rs
+++ src/eventhandler.rs
@@ -173,10 +173,10 @@ fn process_events(me: &mut dyn EventHandler, he: HELEMENT, evtg: UINT, params: L
 
 				_ => {
 					let reason: CLICK_REASON = match nm.reason {
-						0 => CLICK_REASON::BY_MOUSE_CLICK,
-						1 => CLICK_REASON::BY_KEY_CLICK,
-						2 => CLICK_REASON::SYNTHESIZED,
-						3 => CLICK_REASON::BY_MOUSE_ON_ICON,
+						1 => CLICK_REASON::BY_MOUSE_CLICK,
+						2 => CLICK_REASON::BY_KEY_CLICK,
+						3 => CLICK_REASON::SYNTHESIZED,
+						4 => CLICK_REASON::BY_MOUSE_ON_ICON,
 						_ => {
 							eprintln!("[sciter] warning! invalid click reason: {}", nm.reason);
 							return 0;
